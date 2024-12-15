e5463b3ca77d5a72d6ff6fae08202c02b3b9d62c
Bailey <bailey.butler.234@gmail.com>
2024-12-15 19:19:01 +1100

    update template

--- template/1.py
+++ template/1.py
@@ -1,3 +1,4 @@
+import math
 from time import perf_counter_ns
 
 with open("input.txt") as f:
@@ -9,6 +10,8 @@ start = perf_counter_ns()
 for line in lines:
     ...
 
-
 end = perf_counter_ns()
-print(f"duration: {(end-start)/10**6:.2f}ms")
+t = end - start
+precision = min(3 * math.floor(math.log(t) / math.log(10) / 3), 9)
+unit = ["ns", "μs", "ms", "s"][precision//3]
+print(f"duration: {t/10**precision:.3f}{unit}")
